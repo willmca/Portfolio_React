b59fda68239d50b294158b3b9f6f85f3e0b1a554
Will McAllister <willmca32@gmail.com>
2019-10-17 09:47:24 -0400

    Typo fix on homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ class Home extends Component{
                     <p className="intro">Full-stack Software Engineer with a passion for bringing a client's desires to virtual life on the web. I enjoy working directly with people and businesses to help them achieve their goals. I bring to the table a relentless work-ethic and drive to succeed and help others do the same. My skills include React, Responsive Design, CSS, Javascript, Node, Express and more.</p>
                 </div>
                 <div className="skillsHolder">
-                    <p className="skills">When I'm not writing code I enjoy playing gaming, playing basketball, watching my beloved Washington Wizards and Redskins dissapoint me year after year, and hanging out with my dogs, Harley and Daisy. </p>
+                    <p className="skills">When I'm not writing code I enjoy playing gaming, playing basketball, watching my beloved Washington Wizards and Redskins disapoint me year after year, and hanging out with my dogs, Harley and Daisy. </p>
                 </div>
                 <div className="contactHolder">
                     <p className="contact">Please feel free to contact me via email at mcallister.william.r@gmail.com</p>
